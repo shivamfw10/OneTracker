3e1bb6b77dd5972b8b3183e40609f938a94c7b2a
shivamfw10 <akgeeta8874@gmail.com>
2022-07-05 07:59:24 +0530

    ticket status dynmic done

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Router } from '@angular/router';
+import { Ticket } from 'src/app/shared/ticket.model';
+import { TicketsService } from 'src/app/shared/service/tickets.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,9 +10,51 @@ import { Router } from '@angular/router';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor(private router:Router) { }
-
+  data:Ticket[]=[];
+  constructor(private ticketService:TicketsService){}
   ngOnInit(): void {
+    this.ticketService.getTickets().subscribe(response=>{
+      this.data=response;
+      console.log(this.data);
+      console.log(this.data.length)
+    },err=>{
+      console.log(err);
+    })
+  }
+  public countOpenTicket(data:any){
+    let count:number=0;
+    for(let i=0;i<data.length;i++){
+      if(data[i].status=='open'){
+        count++;
+      }
+    }
+    return count;
+  }
+  
+  public countCrossFour(data:any){
+    let count:number =0;
+    for(let i=0;i<data.length;i++){
+      let time = this.timetoNumber(data[i].ticketage)
+      if(time>4){
+        count++;
+      }
+    }
+    return count;
+  }
+
+  public countCloseFour(data:any){
+    let count:number=0;
+    for(let i=0;i<data.length;i++){
+      let time = this.timetoNumber(data[i].ticketage)
+      if(time<4 && data[i].status==='close'){
+        count++;
+      }
+    }
+    return count;
+    
+  }
+  public timetoNumber(data:any){
+    data = data.split(":");
+    return Number(data[0]);
   }
 }
